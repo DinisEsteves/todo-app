333154ec1caed2d9d3c749bf249e406884151c6a
Dinis Esteves <dinisjesteves@gmail.com>
2021-09-20 19:42:43 +0100

    feat: conditional rendering, if component

--- public/js/index.js
+++ public/js/index.js
@@ -181,6 +181,48 @@ var App = function App() {
 
 /***/ }),
 
+/***/ "./resources/js/components/helpers/if/if.jsx":
+/*!***************************************************!*\
+  !*** ./resources/js/components/helpers/if/if.jsx ***!
+  \***************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+
+
+var If = function If(props) {
+  if (!props.hide) {
+    return props.children;
+  } else {
+    return false;
+  }
+};
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (If);
+
+/***/ }),
+
+/***/ "./resources/js/components/helpers/if/index.js":
+/*!*****************************************************!*\
+  !*** ./resources/js/components/helpers/if/index.js ***!
+  \*****************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (/* reexport safe */ _if__WEBPACK_IMPORTED_MODULE_0__.default)
+/* harmony export */ });
+/* harmony import */ var _if__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./if */ "./resources/js/components/helpers/if/if.jsx");
+
+
+/***/ }),
+
 /***/ "./resources/js/components/routes.jsx":
 /*!********************************************!*\
   !*** ./resources/js/components/routes.jsx ***!
@@ -296,17 +338,20 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
-/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react/jsx-runtime */ "./node_modules/react/jsx-runtime.js");
+/* harmony import */ var _helpers_if__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../helpers/if */ "./resources/js/components/helpers/if/index.js");
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! react/jsx-runtime */ "./node_modules/react/jsx-runtime.js");
+
 
 
 
 var IconButton = function IconButton(props) {
-  if (props.hidden) return null;
-  return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)("button", {
-    className: 'btn btn-' + props.style,
-    onClick: props.onClick,
-    children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)("i", {
-      className: 'fa fa-' + props.icon
+  return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx)(_helpers_if__WEBPACK_IMPORTED_MODULE_1__.default, {
+    children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx)("button", {
+      className: 'btn btn-' + props.style,
+      onClick: props.onClick,
+      children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx)("i", {
+        className: 'fa fa-' + props.icon
+      })
     })
   });
 };
--- resources/js/components/template/iconButton/iconButton.jsx
+++ resources/js/components/template/iconButton/iconButton.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import If from '../../helpers/if'
 
-const IconButton = (props) => {
-    if(props.hidden) return null
-    
+const IconButton = (props) => {    
     return(
-        <button className={'btn btn-'+props.style} onClick={props.onClick}>
-            <i className={'fa fa-'+props.icon}></i>
-        </button>
+        <If> 
+            <button className={'btn btn-'+props.style} onClick={props.onClick}>
+                <i className={'fa fa-'+props.icon}></i>
+            </button>
+        </If>
     );
 }
 
